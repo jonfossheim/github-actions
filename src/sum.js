593e993593b2a3c9590f10a44594569467023022
jonfossheim <fossheimjoneven@gmail.com>
2021-02-05 11:35:40 +0100

    lots

---
+++ src/sum.js
@@ -0,0 +1,12 @@
+module.exports = {
+   sum: function sum(a, b) {
+      return a + a;
+   },
+   sum3: function sum3(a, b, c) {
+      return a + b + c;
+   },
+};
+
+const sum4 = (a, b, c, d) => {
+   return a + b + c + d;
+};
